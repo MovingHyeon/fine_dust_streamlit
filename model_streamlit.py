2efd8d08855d6d89fb1d9c6d428ff028171779a6
Donghyeon Lee <69422473+MovingHyeon@users.noreply.github.com>
2024-05-25 12:07:50 +0900

    Update model_streamlit.py

--- model_streamlit.py
+++ model_streamlit.py
@@ -214,7 +214,7 @@ with mapbox:
     #     st.markdown(css, unsafe_allow_html=True)
     #     st_folium(map, use_container_width = True, height = 600)
 
-    st.components.v1.html(map._repr_html_(), width=600, height=400)
+    st.components.v1.html(map._repr_html_(), width=650, height=450)
 
 
     st.bar_chart(
